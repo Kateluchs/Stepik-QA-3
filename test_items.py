107864cb130cccf1dae28b09caa3a358c3fb0711
User <kateluchs@gmail.com>
2020-02-11 17:10:25 +0300

    add assert

--- test_items.py
+++ test_items.py
@@ -1,10 +1,11 @@
-import time
+from selenium.common.exceptions import NoSuchElementException
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-
 
 def test_add_to_bascket_button_is_here(browser):
     browser.get(link)
-    browser.find_element_by_class_name("btn-add-to-basket")
-    time.sleep(10)
-
+    try:
+        browser.find_element_by_xpath("//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
+    except NoSuchElementException:
+        assert False, "button is not found"
     #pytest -s -v --browser_name=firefox --language=es test_items.py
+    #pytest -s -v --language=es test_items.py
